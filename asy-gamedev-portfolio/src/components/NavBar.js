0cf55f723f9ae3b7138a09776190d0c9c3486797
Antonio Sarmiento <antoniosaryon155@gmail.com>
2024-02-21 15:51:01 -0500

    Banner Ready

--- asy-gamedev-portfolio/src/components/NavBar.js
+++ asy-gamedev-portfolio/src/components/NavBar.js
@@ -3,15 +3,18 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
-import navIcon3 from '../assets/img/nav-icon3.svg'
+import linkedinIcon from '../assets/img/nav-icon1.svg'
+import itchioIcon from '../assets/img/itchio.png'
+
+// ------------------------------------------------------------
 
 export const NavBar = () => {
     
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
     
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
     useEffect(() => {
 
         const onScroll = () => {
@@ -26,10 +29,14 @@ export const NavBar = () => {
 
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
     
     const onUpdateActiveLink = (value) => {
         setActiveLink(value);
     }
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
     return(
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
@@ -40,17 +47,16 @@ export const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
-            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
-            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Inicio</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Habilidades</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Proyectos</Nav.Link>
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
-                    <a href="#"><img src={navIcon1} alt="navIcon1"/></a>
-                    <a href="#"><img src={navIcon2} alt="navIcon2"/></a>
-                    <a href="#"><img src={navIcon3} alt="navIcon3"/></a>
+                    <a href="https://www.linkedin.com/in/antonio-sarmiento-yon-5a44b01a5/" target='_blank'><img src={linkedinIcon} alt="navIcon1"/></a>
+                    <a href="https://larraondo.itch.io" target='_blank'><img src={itchioIcon} alt="navIcon3"/></a>
                 </div>
-                <button className='vvd' onClick={() => console.log('connect')}/><span>Conectémos</span>
+                <button className='vvd' onClick={() => console.log('connect')}><span>Conectémos</span></button>
             </span>
         </Navbar.Collapse>
         </Container>
